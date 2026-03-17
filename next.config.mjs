247cb4a639ab5288433cfbeeddc6489554abe974
alnazeerabdo <alnzyrbdalmnm90@gmail.com>
2026-03-17 09:45:49 +0200

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Tells Next.js to export static files
+
+  // UNCOMMENT the line below and change 'sanad-AI' to your actual GitHub repository name if it's not hosted at the root domain!
+  basePath: '/sanad-AI',
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // This is required for Next.js images on GitHub Pages
   },
 }
 
